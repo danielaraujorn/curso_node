bb18d111206fcfba0994858cfd5e17319ac026bc
apenaz <patricio@bct.ect.ufrn.br>
2018-01-17 19:18:57 -0300

    tratando request manualmente

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-var express = require('express');
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+
+	var categoria = req.url;
+	if (categoria == '/tecnologia') {
+	
+		res.end("<html><body>Notícias de tecnologia</body></html>");
+	
+	} else if (categoria == '/moda') {
+
+		res.end("<html><body>notícias de moda</body></html>");
+
+	} else if (categoria == '/beleza') {
+
+		res.end("<html><body>Notícias de beleza</body></html>");
+
+	} else {
+	
+	res.end("<html><body>Portal de notícias</body></html>");
+
+	}
+
+
+});
+
+
+
+
+
+server.listen(3000);
