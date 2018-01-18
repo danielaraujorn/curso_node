2d51d5e94de833872cd1ee6c022d892358a24c9d
apenaz <patricio@bct.ect.ufrn.br>
2018-01-18 01:16:40 -0300

    conecção com banco de dados e exibição ok

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ var rotaNoticias = require('./app/routes/noticias.js')(app);
 
 var rotaFormIncNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
 
-app.listen(8080, function() {
+app.listen(3000, function(){
 	console.log("Server ON");
 });
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,10 +1,25 @@
 module.exports = function(application) {
 	
 	application.get('/noticias',function(req, res) {
-		res.render("noticias/noticias");
+	
+		var mysql = require('mysql');
+
+		var connection = mysql.createConnection({
+			host : 'localhost',
+			user : 'root',
+			password : '1234',
+			database : 'portalnoticias'
+		});
+
+		connection.query('select * from noticias', function(error, result){
+			//res.send(result);
+			res.render("noticias/noticias", {noticias : result});
+		});
+
+
 	});
 
-//	application.get('/noticia', function (req, res) {
-//		res.render("noticias/noticia");
-//	});
+	//	application.get('/noticia', function (req, res) {
+	//		res.render("noticias/noticia");
+	//	});
 }
