db6f3b10630b8df54fa7bccc61039ded1a398181
apenaz <patricio@bct.ect.ufrn.br>
2018-01-18 06:11:03 -0300

    inclusão da model noticiasModel e rotas para salvarNoticias e inclusão no banco de dados

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 var app = require('./config/server');
 
-var rotaHome = require('./app/routes/home.js')(app);
-
-var rotaNoticias = require('./app/routes/noticias.js')(app);
-
-var rotaFormIncNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
-
 app.listen(3000, function(){
 	console.log("Server ON");
 });
--- config/server.js
+++ config/server.js
@@ -1,8 +1,18 @@
 var express = require('express');
+var consign =  require('consign');
+var bodyParser = require('body-parser');
 
 var app = express();
-
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+consign()//já executa as funções retornadas
+	.include('./app/routes')
+	.then('config/dbConnection.js')//neste caso precisa colocar a extensão pra entender que é módulo e não diretorio .js ...
+	.then('./app/models')
+	.into(app);
+
 
 module.exports = app;
