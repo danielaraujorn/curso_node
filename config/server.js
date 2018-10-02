a49f87a48a06142ba50ebe052b8aa4ea08270927
Daniel Araújo <daniel.araujorn01@gmail.com>
2018-10-02 11:57:41 -0300

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,10 +1,8 @@
-var express = require('express');
+var app = require('express')();
 var consign =  require('consign');
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
 
-
-var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
